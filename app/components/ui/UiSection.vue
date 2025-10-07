799e68a6ca9fe3a9c3d0412a121cf99c810aa8b4
Thorsten Seyschab <business@todde.tv>
2025-10-07 15:06:11 +0200

    feat: Add core view mode to results page with customization params (#14)

--- app/components/ui/UiSection.vue
+++ app/components/ui/UiSection.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  bare: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
 <template>
-  <section class="bg-white border-[3px] border-black p-8">
+  <section :class="{ 'bg-white border-[3px] border-black p-8' : !bare }">
     <slot />
   </section>
 </template>
