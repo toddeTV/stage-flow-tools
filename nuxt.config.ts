d6b578bc1806ebac4429ba45e955e3f9c5fbe1e2
Thorsten Seyschab <business@todde.tv>
2025-10-07 09:54:09 +0200

    refactor: rename websocket debug variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
       host: '0.0.0.0',
       port: '3000',
       debug: {
-        showWebsockets: false
+        showWebsocketConnectionsInFrontend: false,
       }
     }
   },
