32f60dea9a38bade4e69e85d410924c392bd14d8
Thorsten Seyschab <business@todde.tv>
2026-03-10 15:36:12 +0100

    feat: add UiCheckbox reusable component

---
+++ app/components/ui/UiCheckbox.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  size: {
+    type: String,
+    default: 'medium' // 'medium' or 'small'
+  }
+})
+
+const emit = defineEmits<{
+  'update:modelValue': [value: boolean]
+}>()
+
+function handleChange(event: Event) {
+  const target = event.target as HTMLInputElement
+  emit('update:modelValue', target.checked)
+}
+
+const sizeClasses = computed(() => {
+  return {
+    'p-5': props.size === 'medium',
+    'p-2': props.size === 'small',
+    'text-lg': props.size === 'medium',
+    'text-sm': props.size === 'small'
+  }
+})
+
+const inputSizeClasses = computed(() => {
+  return {
+    'w-5 h-5 mr-4': props.size === 'medium',
+    'w-4 h-4 mr-2': props.size === 'small'
+  }
+})
+</script>
+
+<template>
+  <label
+    class="flex items-center border-[3px] border-black cursor-pointer transition-all duration-200 relative"
+    :class="[
+      sizeClasses,
+      {
+        'bg-black text-white': modelValue,
+        'opacity-60 cursor-not-allowed': disabled,
+        'hover:translate-x-1 hover:shadow-[-5px_5px_0_#000]': !disabled
+      }
+    ]"
+  >
+    <input
+      type="checkbox"
+      :checked="modelValue"
+      :disabled="disabled"
+      class=""
+      :class="[inputSizeClasses, modelValue ? 'accent-white' : 'accent-black']"
+      @change="handleChange"
+    >
+    <span class="font-bold">
+      <slot />
+    </span>
+  </label>
+</template>
