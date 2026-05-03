72957388dce37a331cb18785f60236236b265d78
Thorsten Seyschab <hello@todde.tv>
2026-05-03 15:39:44 +0200

    chore: lint project config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,60 +2,15 @@ import { version } from './package.json'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt',
-  ],
 
-  ssr: false,
-
-  devtools: {
-    enabled: true,
-  },
+  compatibilityDate: '2025-07-15',
 
   css: [
     '~/assets/css/main.css',
   ],
 
-  runtimeConfig: {
-    // Private keys (only available server-side)
-    adminUsername: 'admin',
-    adminPassword: '123',
-    jwtSecret: 'tryUJ0zQbstPbTOrezme+Fv+KndzDNRx5lmSeelr2ial2/2yV8HqLeQ2felJafqf',
-
-    // Public keys (available on both client and server)
-    public: {
-      wsUrl: '',
-      apiUrl: '',
-      host: '0.0.0.0',
-      port: '3000',
-      debug: {
-        showWebsocketConnectionsInFrontend: false,
-        showConsoleOutputs: false,
-      },
-      version: version,
-      emojiCooldownMs: 1500,
-    },
-  },
-
-  compatibilityDate: '2025-07-15',
-
-  nitro: {
-    experimental: {
-      websocket: true,
-    },
-    storage: {
-      data: { driver: 'fs', base: './.data/db' },
-    },
-    devStorage: {
-      data: { driver: 'fs', base: './.data/db' },
-    },
-  },
-
-  typescript: {
-    shim: false,
+  devtools: {
+    enabled: true,
   },
 
   eslint: { // for `@nuxt/eslint`
@@ -68,15 +23,60 @@ export default defineNuxtConfig({
   },
 
   i18n: {
+    defaultLocale: 'en',
     locales: [
       { code: 'en', language: 'en-US', name: 'English' },
       { code: 'de', language: 'de-DE', name: 'Deutsch' },
       { code: 'ja', language: 'ja-JP', name: '日本語' },
     ],
-    defaultLocale: 'en',
     vueI18n: './i18n.config.ts', // Using a separate file for better organization
   },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+  ],
+
+  nitro: {
+    devStorage: {
+      data: { base: './.data/db', driver: 'fs' },
+    },
+    experimental: {
+      websocket: true,
+    },
+    storage: {
+      data: { base: './.data/db', driver: 'fs' },
+    },
+  },
+
+  runtimeConfig: {
+    // Private keys (only available server-side)
+    adminPassword: '123',
+    adminUsername: 'admin',
+    jwtSecret: 'tryUJ0zQbstPbTOrezme+Fv+KndzDNRx5lmSeelr2ial2/2yV8HqLeQ2felJafqf',
+
+    // Public keys (available on both client and server)
+    public: {
+      apiUrl: '',
+      debug: {
+        showConsoleOutputs: false,
+        showWebsocketConnectionsInFrontend: false,
+      },
+      emojiCooldownMs: 1500,
+      host: '0.0.0.0',
+      port: '3000',
+      version,
+      wsUrl: '',
+    },
+  },
+
+  ssr: false,
 
   tailwindcss: { // for Nuxt module `@nuxtjs/tailwindcss`
+  },
+
+  typescript: {
+    shim: false,
   },
 })
